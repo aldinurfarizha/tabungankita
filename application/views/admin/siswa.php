3407a1a3bcd126cfe40aabf63399904dddee1f4d
aldinurfarizha <aldinurfarizha@gmail.com>
2019-09-26 02:06:33 +0700

    nya

--- application/views/admin/siswa.php
+++ application/views/admin/siswa.php
@@ -52,7 +52,7 @@
 	<td><?php echo $sws->alamat?></td>
 	<td><?php echo $sws->telepon ?></td>
 	<td onclick="javascript: return confirm('Anda Yakin Hapus ? ')"><?php echo anchor ('admin/siswa/hapus/'.$sws->id_siswa, '<div class="btn btn-danger btn-sm"><i class ="fa fa-trash"></i></div>')?></td>
-        <td><button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-edit"></i></button></td>
+        <td><button type="button" class="btn btn-warning" data-toggle="modal" data-target="#asd<?php echo $sws->id_siswa?>"><i class="fa fa-edit"></i></button></td>
 		
 </tr>
 <?php endforeach; ?>
@@ -67,7 +67,8 @@
 	<!-- Button trigger modal -->
 
 
-<!-- Modal -->
+<!-- Modal nu kahiji-->
+
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -107,6 +108,56 @@
     </div>
   </div>
 </div>
+</div>
+
+<!-- Modal nu kadua-->
+<?php
+                    foreach ($siswa as $a) {
+                        $id=$a['id_siswa'];
+                     
+                    ?>
+<div class="modal asd" id="asd<?php echo $sws->id_siswa?>" tabindex="-1" role="dialog" aria-labelledby="asd" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="asd">Edit Siswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="<?php echo base_url().'admin/siswa/tambah_aksi'; ?>">
+		<div class="form-group">
+		<label>NIS</label>
+		<input type="text" name="id_siswa" class="form-control">
+		<label>Nama Siswa</label>
+		<input type="text" name="nama" class="form-control">
+		<label>Alamat</label>
+		<input type="text" name="alamat" class="form-control">
+		<label>Telepon</label>
+		<input type="number" name="telepon" class="form-control">
+		<label>Jenis Kelamin</label>
+		<div class="radio">
+			<label>
+				<input type="radio" name="jenis_kelamin" value="Laki-laki" checked>Laki-laki
+			</label>
+		</div>
+		<div class="radio">
+			<label>
+				<input type="radio" name="jenis_kelamin" value="Perempuan">Perempuan
+			</label>
+		</div>
+      </div>
+      <div class="modal-footer">
+        <button type="reset" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+        }
+        ?>
 </body>
 </html>
 
